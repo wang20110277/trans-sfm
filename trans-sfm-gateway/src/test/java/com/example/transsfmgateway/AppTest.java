f4fae307f329f6301a805c8470ebac0ac8d4cbb4
wanghuabing <wanghuabing@tal.com>
2025-08-07 12:54:15 +0800

    test: 删除 AppTest 类中的冗余测试代码
    - 移除了 AppTest 类中不必要的构造函数、suite 方法和 testApp 测试方法
    - 保留了 contextLoads测试方法，用于验证应用上下文是否正确加载

--- trans-sfm-gateway/src/test/java/com/example/transsfmgateway/AppTest.java
+++ trans-sfm-gateway/src/test/java/com/example/transsfmgateway/AppTest.java
@@ -5,31 +5,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class AppTest {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
 
     @Test
     public void contextLoads() {
